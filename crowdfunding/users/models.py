2f72de5cf43b8d5eecccabcb37d0998a2610d90b
kalicialopez <104682798+kalicialopez@users.noreply.github.com>
2023-01-25 19:09:04 +0800

    Fixed deployment

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=200, null=False, blank=False)
     last_name = models.CharField(max_length=200, null=False, blank=False)
-    date_of_birth = models.DateField(null=False, blank=False)
-    profile_picture = models.URLField(blank=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    profile_picture = models.URLField(blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
-    country_of_residence = models.CharField(max_length=200, blank=False)
-    highest_level_of_education = models.CharField(max_length=200, null=False, blank=False)
+    country_of_residence = models.CharField(max_length=200, blank=True, null=True)
+    highest_level_of_education = models.CharField(max_length=200, blank=True, null=True)
 
 
     def __str__(self):
